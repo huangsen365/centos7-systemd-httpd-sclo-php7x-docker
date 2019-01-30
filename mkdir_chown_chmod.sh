3ca89524a351efe8a2ec2763a4951713c3a76ab4
root <root@ip-172-31-44-37.ap-southeast-1.compute.internal>
2019-01-30 04:05:52 +0000

    modified:   mkdir_chown_chmod.sh 	modified:   yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh

--- mkdir_chown_chmod.sh
+++ mkdir_chown_chmod.sh
@@ -13,3 +13,4 @@ chown -R root:apache /var/opt/rh/rh-php71/lib/php/*
 #mkdir /var/www/www.yourdomain.com
 #su - sshuser -c 'echo "<?php phpinfo(); ?>" > /var/www/www.yourdomain.com/info.php'
 #chown sshuser:sshuser /var/www/www.yourdomain.com
+#chown 1000:1000 /var/www/www.yourdomain.com
--- yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh
+++ yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh
@@ -1,1 +1,5 @@
 docker exec -it centos7-systemd-httpd-sclo-php7x-docker_yourdomain.com /bin/sh /root/mkdir_chown_chmod.sh
+mkdir ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com
+echo "<?php phpinfo(); ?>" > ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/info.php
+chown 1000:1000 ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com
+
