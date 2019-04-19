44b69aa87ee3b8a20732baf487e352f47ec8831b
huangsen365 <huangsen365@gmail.com>
2019-04-19 13:22:55 +0800

    Add files via upload

--- gitpull.php
+++ gitpull.php
@@ -1,14 +1,9 @@
-<?php 
+<?php
 
 //exec("GIT_DIR=/var/www/www.yourdomain.com/.git git pull", $output);
-exec("sh /var/www/" . $_SERVER['SERVER_NAME'] . "/gitpull.sh", $output);
-
-
+$git_pull_command = "sh /var/www/" . $_SERVER['SERVER_NAME'] . "/gitpull.sh";
+print $git_pull_command;
+exec($git_pull_command, $output);
 echo '<pre>';
 print_r($output);
 echo '</pre>';
-
-
-
-
-?>
