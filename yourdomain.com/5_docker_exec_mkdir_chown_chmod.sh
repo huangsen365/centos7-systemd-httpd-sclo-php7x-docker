fdf581f5a1f60b5f914576ccf3c795d44f43ed10
huangsen365 <huangsen365@gmail.com>
2019-04-22 00:58:58 +0800

    Update 5_docker_exec_mkdir_chown_chmod.sh

--- yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh
+++ yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh
@@ -1,5 +1,5 @@
 docker exec -it centos7-systemd-httpd-sclo-php7x-docker_yourdomain.com /bin/sh /root/mkdir_chown_chmod.sh
-mkdir ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com
+mkdir -p ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/www
 echo "<?php phpinfo(); ?>" > ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/info.php
 chown -R 1000:1000 ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com
 
