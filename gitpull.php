547483196f04b403bd4da09a052b7f9ce1838fb4
huangsen365 <huangsen365@gmail.com>
2019-05-11 13:14:26 +0800

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,9 +1,13 @@
 <?php
 
-//exec("GIT_DIR=/var/www/www.yourdomain.com/.git git pull", $output);
 $git_pull_command = "sh /var/www/" . $_SERVER['SERVER_NAME'] . "/gitpull.sh";
 print $git_pull_command;
 exec($git_pull_command, $output);
 echo '<pre>';
 print_r($output);
 echo '</pre>';
+
+echo "Updated time is " . date("Y/m/d H:i:s") . "<br>";
+echo "Today is " . date("l") . "<br>";
+
+echo "<a href='" . $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . "'>" . $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . "</a><br>";
