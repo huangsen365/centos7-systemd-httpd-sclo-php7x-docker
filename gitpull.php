4970c7f3c8cce07d4d7895d60e91dfe79994c733
huangsen365 <huangsen365@gmail.com>
2019-11-13 02:24:09 +0800

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,7 @@
 <p>
 <?php
 
+/* Please add this into the .gitignore file */
 $git_pull_command = "sh /var/www/" . $_SERVER['SERVER_NAME'] . "/gitpull.sh";
 print $git_pull_command;
 exec("git config --get remote.origin.url", $output);
