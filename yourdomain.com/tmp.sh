3974477b273fe98f1553bfe63ca9c0164b8a294d
root <root@ip-172-31-47-246.ap-east-1.compute.internal>
2019-11-12 17:48:12 +0000

    modified:   tmp.sh

--- yourdomain.com/tmp.sh
+++ yourdomain.com/tmp.sh
@@ -5,5 +5,7 @@ sh ./3_docker_stop_and_rm_mycentos.sh
 sh ./4_docker_run_centos7-systemd-httpd-sclo-php7x-docker_yourdomain.com.sh
 sh ./5_docker_exec_mkdir_chown_chmod.sh
 sh ./6_docker_exec_it_bash.sh
+cp ../gitpull.php ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/www/
+cp ../gitpull.sh ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/
 #sh ./x_1_docker_stop_and_rm_centos7-systemd-httpd-sclo-php7x-docker_yourdomain.com.sh
 #sh ./x_2_rm_default_paths_for_docker.sh
