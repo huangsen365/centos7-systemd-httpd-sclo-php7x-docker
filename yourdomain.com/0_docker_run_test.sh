d58e42a0b355a2791a82f793817ad5d42355b850
huangsen365 <huangsen365@gmail.com>
2019-11-13 16:53:23 +0800

    Update 0_docker_run_test.sh

--- yourdomain.com/0_docker_run_test.sh
+++ yourdomain.com/0_docker_run_test.sh
@@ -1,1 +1,2 @@
-docker run --privileged --name centos7-systemd-httpd-sclo-php7x-test -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -d --restart always centos7-systemd-httpd-sclo-php7x
+#docker run --privileged --name centos7-systemd-httpd-sclo-php7x-test -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -d --restart always centos7-systemd-httpd-sclo-php7x
+docker run --privileged --name centos7-systemd-httpd-sclo-php7x-test -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d --restart always centos7-systemd-httpd-sclo-php7x
