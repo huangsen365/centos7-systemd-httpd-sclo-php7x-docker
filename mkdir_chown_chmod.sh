8c500a5978d85d5b91dd54769cb6d8fe675ae50c
huangsen365 <huangsen365@gmail.com>
2019-11-22 18:57:47 +0800

    Update mkdir_chown_chmod.sh

--- mkdir_chown_chmod.sh
+++ mkdir_chown_chmod.sh
@@ -14,3 +14,18 @@ chown -R root:apache /var/opt/rh/rh-php71/lib/php/*
 #su - sshuser -c 'echo "<?php phpinfo(); ?>" > /var/www/www.yourdomain.com/info.php'
 #chown sshuser:sshuser /var/www/www.yourdomain.com
 #chown 1000:1000 /var/www/www.yourdomain.com
+
+mkdir /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
+#chown -R root:apache /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
+chmod 770 /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
+
+mkdir /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com
+#chown -R root:apache /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com
+chmod 770 /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com
+
+chown -R root:apache /var/opt/rh/rh-php70/lib/php/*
+
+#mkdir /var/www/www.yourdomain.com
+#su - sshuser -c 'echo "<?php phpinfo(); ?>" > /var/www/www.yourdomain.com/info.php'
+#chown sshuser:sshuser /var/www/www.yourdomain.com
+#chown 1000:1000 /var/www/www.yourdomain.com
