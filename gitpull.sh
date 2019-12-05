ed9bf3f9d0e9c7cdec80f17db7beb8332730b5dc
huangsen365 <huangsen365@gmail.com>
2019-12-05 10:29:36 +0800

    Update gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -1,2 +1,3 @@
 # Please add 'gitpull.sh' into the .gitignore file
+# wget https://raw.githubusercontent.com/huangsen365/centos7-systemd-httpd-sclo-php7x-docker/master/gitpull.sh -O gitpull.sh
 git pull
